9af35de8dc4bb12a0ca0f32c0e4604cbc220f172
curder <q.curder@gmail.com>
2022-01-05 18:56:58 +0800

    Load the Transcript as an Array

--- src/Transcription.php
+++ src/Transcription.php
@@ -8,19 +8,34 @@ namespace Curder\PhpPackageTranscriptionsDemo;
  */
 class Transcription
 {
-    protected string $file;
+    protected array $lines;
 
-    public static function load(string $path): string
+    public static function load(string $path): self
     {
         $instance = new static;
 
-        $instance->file = file_get_contents($path);
+        $instance->lines = $instance->discardsIrrelevantLines(file($path));
 
         return $instance;
     }
 
+    public function lines(): array
+    {
+        return $this->lines;
+    }
+
     public function __toString(): string
     {
-        return $this->file;
+        return implode('', $this->lines);
+    }
+
+    protected function discardsIrrelevantLines(array $lines) : array
+    {
+        $lines = array_map('rtrim', $lines);
+
+        return array_filter(
+            $lines,
+            fn ($line) => $line !== 'WEBVTT' && $line !== '' && !is_numeric($line)
+        );
     }
 }
--- tests/TranscriptionTest.php
+++ tests/TranscriptionTest.php
@@ -11,6 +11,28 @@ class TranscriptionTest extends TestCase
     {
         $file = __DIR__.'/stubs/basic-example.vtt';
 
-        $this->assertStringEqualsFile($file, Transcription::load($file));
+        $transcription = Transcription::load($file);
+
+        $this->assertStringContainsString('Here is a', $transcription);
+        $this->assertStringContainsString('example of a VTT file.', $transcription);
+    }
+
+    /** @test */
+    public function it_can_convert_to_an_array_of_lines(): void
+    {
+        $file = __DIR__.'/stubs/basic-example.vtt';
+
+        $this->assertCount(4, Transcription::load($file)->lines());
+    }
+
+    /** @test */
+    public function it_discards_irrelevant_lines_from_the_vtt_file(): void
+    {
+        $file = __DIR__ . '/stubs/basic-example.vtt';
+
+        $transaction = Transcription::load($file);
+
+        $this->assertStringNotContainsString('WEBVTT', $transaction);
+        $this->assertCount(4, $transaction->lines());
     }
 }

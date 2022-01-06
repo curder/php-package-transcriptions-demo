e757be3a735dfcfbc6fe82a1657e122667cd6822
curder <q.curder@gmail.com>
2022-01-06 10:28:43 +0800

    Refactor Collections

---
+++ src/Collection.php
@@ -0,0 +1,63 @@
+<?php
+namespace Curder\PhpPackageTranscriptionsDemo;
+
+use ArrayIterator;
+use ArrayAccess;
+use Countable;
+use IteratorAggregate;
+use JsonSerializable;
+
+/**
+ * Class Collection
+ *
+ * @package \Curder\PhpPackageTranscriptionsDemo
+ */
+class Collection implements Countable, IteratorAggregate, ArrayAccess, JsonSerializable
+{
+    protected array $items;
+
+    public function __construct(array $items)
+    {
+        $this->items = $items;
+    }
+
+    public function map(callable $fn): self
+    {
+        return new static(
+            array_map($fn, $this->items)
+        );
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->items;
+    }
+    public function offsetUnset($key)
+    {
+        unset($this->items[$key]);
+    }
+    public function offsetGet($key)
+    {
+        return $this->items[$key];
+    }
+    public function offsetExists($key) : bool
+    {
+        return isset($this->items[ $key ]);
+    }
+    public function getIterator() : ArrayIterator
+    {
+        return new ArrayIterator($this->items);
+    }
+    public function offsetSet($key, $value)
+    {
+        if (is_null($key)) {
+            $this->items[] = $value;
+        } else {
+            $this->items[$key] = $value;
+        }
+    }
+    public function count() : int
+    {
+        return count($this->items);
+    }
+}
--- src/Lines.php
+++ src/Lines.php
@@ -2,77 +2,24 @@
 
 namespace Curder\PhpPackageTranscriptionsDemo;
 
-use ArrayAccess;
-use ArrayIterator;
-use Countable;
-use IteratorAggregate;
-use JsonSerializable;
+
+
+use phpDocumentor\Reflection\Types\This;
 
 /**
  * Class Lines
  *
  * @package \Curder\PhpPackageTranscriptionsDemo
  */
-class Lines implements Countable, IteratorAggregate, ArrayAccess, JsonSerializable
+class Lines extends Collection
 {
-    protected array $lines;
-
-    public function __construct(array $lines)
-    {
-        $this->lines = $lines;
-    }
-
     public function asHtml(): string
     {
-        $formattedLines = array_map(
-            static fn (Line $line) => $line->toAnchorTag(),
-            $this->lines,
-        );
-
-        return (new static($formattedLines))->__toString();
-    }
-
-    public function count(): int
-    {
-        return count($this->lines);
-    }
-
-    public function getIterator(): ArrayIterator
-    {
-        return new ArrayIterator($this->lines);
-    }
-
-    public function offsetExists($key): bool
-    {
-        return isset($this->lines[$key]);
-    }
-
-    public function offsetGet($key)
-    {
-        return $this->lines[$key];
-    }
-
-    public function offsetSet($key, $value)
-    {
-        if (is_null($key)) {
-            $this->lines[] = $value;
-        } else {
-            $this->lines[$key] = $value;
-        }
-    }
-
-    public function offsetUnset($key)
-    {
-        unset($this->lines[$key]);
-    }
-
-    public function jsonSerialize()
-    {
-        return $this->lines;
+        return $this->map(fn (Line $line) => $line->toAnchorTag())->__toString();
     }
 
     public function __toString(): string
     {
-        return implode(PHP_EOL, $this->lines);
+        return implode(PHP_EOL, $this->items);
     }
 }
